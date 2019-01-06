0d7f2a322531cdcce13f1068e0f6e0f4f15eae00
Alan <alanzhang93@gmail.com>
2019-01-06 03:13:30 -0500

    some problems

---
+++ DMOJ/year2019p2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define MAXN 0xF4243
+#define MAXM 0x3B9ACA03
+
+using namespace std;
+
+int N, M;
+pair<int, int> children[MAXN];
+bitset<MAXN> state;
+bitset<MAXN> dp[10];
+
+
+void test(int idx) {
+     if(idx == N + 1) return;
+     
+     state[idx].flip();
+
+     state[idx] ? test(children[idx].first) : test(children[idx].second);
+
+     return;
+}
+
+int main() {
+     cin >> N >> M;
+     
+     children[0] = {1, 1};
+
+     for(int i = 1, a, b; i < N + 1; i++) {
+          scanf("%d %d", &a, &b);
+	  children[i] = {a, b};
+     }
+
+     test(0);
+
+     for(int i = 1; i < M; i++) {
+          if(state.count() == 0) { state = dp[M % i]; break; } 
+          
+	  dp[i % 10] = state;
+	  test(0);
+     }
+
+     for(int i = 1; i < N + 1; i++) {
+          printf("%d", state[i] ? 1 : 0);
+     }
+
+     printf("\n");
+
+     return 0;
+}
