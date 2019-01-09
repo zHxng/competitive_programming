fc5f66ed34520eb41a1bd5ba4ced4fc0ed5f5bc4
Alan Zhang <Alan@v1020-wn-125-147.campus-dynamic.uwaterloo.ca>
2019-01-09 11:16:17 -0500

    formatting

--- CCC/CCC01S3.cpp
+++ CCC/CCC01S3.cpp
@@ -4,62 +4,63 @@ using namespace std;
 
 bool adjmatrix[27][27];
 
-bool disconnect(){
-	bool visited[27];
-	queue<int> q;
+bool disconnect() {
+    bool visited[27];
+    queue<int> q;
 
-	while(!q.empty()) { q.pop(); }
-	memset(visited, 0, sizeof visited);
+    while(!q.empty()) { q.pop(); }
+    memset(visited, 0, sizeof visited);
 
-	q.push(0);
+    q.push(0);
 
-	while(!q.empty()) {
-		int current = q.front();
-		q.pop();
+    while(!q.empty()) {
+	int current = q.front();
+	q.pop();
 
-		if(current == 1) return false;
+	if(current == 1) return false;
 
-		visited[current] = true;
+	visited[current] = true;
 
-		for(int i = 0; i < 27; i++){
-			if((adjmatrix[current][i] || adjmatrix[i][current]) && !visited[i]) {
-				q.push(i);
-			}
-		}
+	for(int i = 0; i < 27; i++){
+	    if((adjmatrix[current][i] || adjmatrix[i][current]) && !visited[i]) {
+    		q.push(i);
+	    }
 	}
+    }
 
-	return true;
+    return true;
 }
 
 int main() {
-	queue<pair<int, int>> q;
+    queue<pair<int, int>> q;
 
-	while(true) {
-		string s;
-		cin >> s;
-		if(s == "**") break;
+    while(true) {
+	string s;
+	cin >> s;
+	
+        if(s == "**") break;
 
-		adjmatrix[s[0] - 'A'][s[1] - 'A'] = true;
-		q.push({s[0] - 'A', s[1] - 'A'});
+        adjmatrix[s[0] - 'A'][s[1] - 'A'] = true;
+	q.push({s[0] - 'A', s[1] - 'A'});
+    }
+
+    int solns = 0;
+    while(!q.empty()) {
+    	pair<int, int> r = q.front();
+    	q.pop();
+
+    	int a = r.first;
+        int b = r.second;
+
+	adjmatrix[a][b] = false;
+
+	if (disconnect()) {
+	    cout << (char) (a + 'A') << (char) (b + 'A') << endl;
+            solns++;
 	}
 
-	int solns = 0;
-	while(!q.empty()) {
-		pair<int, int> r = q.front();
-		q.pop();
+	adjmatrix[a][b] = true;
+    }
 
-		int a = r.first;
-		int b = r.second;
-
-		adjmatrix[a][b] = false;
-
-		if (disconnect()) {
-			cout << (char) (a + 'A') << (char) (b + 'A') << endl;
-			solns++;
-		}
-
-		adjmatrix[a][b] = true;
-	}
-
-	cout << "There are " << solns << " disconnecting roads." << endl;
+    cout << "There are " << solns << " disconnecting roads." << endl;
 }
--- DMOJ/year2019p2.cpp
+++ DMOJ/year2019p2.cpp
@@ -8,47 +8,47 @@ int N, M;
 pair<int, int> children[MAXN];
 bool state[MAXN];
 int height[MAXN];
-
+    
 void sim() {
-     int idx = 1;
+    int idx = 1;
 
-     while (idx != N + 1) {
-          state[idx] ^= 1;
-          if(state[idx]) idx = children[idx].first;
-          else idx = children[idx].second;
-     }
+    while (idx != N + 1) {
+        state[idx] ^= 1;
+        if(state[idx]) idx = children[idx].first;
+        else idx = children[idx].second;
+    }
 }
 
 int max_height(int idx) {
-     if(idx == N + 1) return 0;
-     if(height[idx] != -1) return height[idx];
+    if(idx == N + 1) return 0;
+    if(height[idx] != -1) return height[idx];
      
-     int ans = max(max_height(children[idx].first), max_height(children[idx].second)) + 1;
-     height[idx] = ans;
-     return ans;
+    int ans = max(max_height(children[idx].first), max_height(children[idx].second)) + 1;
+    height[idx] = ans;
+    return ans;
 }
 
 int main() {
-     scanf("%d%d", &N, &M);
+    scanf("%d%d", &N, &M);
 
-     for(int i = 1, a, b; i < N + 1; i++) {
-          scanf("%d%d", &a, &b);
-          children[i] = make_pair(a, b);
-     }
+    for(int i = 1, a, b; i < N + 1; i++) {
+        scanf("%d%d", &a, &b);
+        children[i] = make_pair(a, b);
+    }
 
-     memset(height, -1, sizeof height);
-     memset(state, 0, sizeof state);
+    memset(height, -1, sizeof height);
+    memset(state, 0, sizeof state);
 
-     int h = max_height(1);
-     int cyc = M % (1 << h);
+    int h = max_height(1);
+    int cyc = M % (1 << h);
      
-     for(int i = 0; i < cyc; i++) {
-          sim();
-     }
+    for(int i = 0; i < cyc; i++) {
+        sim();
+    }
 
-     for(int i = 1; i < N + 1; i++) {
-          printf("%d", state[i]);
-     }
+    for(int i = 1; i < N + 1; i++) {
+        printf("%d", state[i]);
+    }
 
-     printf("\n");
+    printf("\n");
 }
